bb370762d40442fae0d9dcfbe6dd489be9c5dea1
CRWill97 <91710270+CRWill97@users.noreply.github.com>
2023-11-19 23:00:42 -0500

    Update streamlit_dash_genai005.py

--- streamlit_dash_genai005.py
+++ streamlit_dash_genai005.py
@@ -31,7 +31,7 @@ if openai_api_key:
     uploaded_file = st.file_uploader("Upload CSV file", type="csv")
     
     if uploaded_file is not None:
-        data = pd.read_csv(uploaded_file)
+        data = pd.read_csv(uploaded_file, sep=',')
         st.write(data.head())
 
         available_columns = data.columns
